8546f7ca6f3d8c6605431031b31f975caa1d04be
Hana <rehanaps323@gmail.com>
2026-09-04 16:06:27 -0500

    Delta table creation

--- azurestoragepractice/read_data.py
+++ azurestoragepractice/read_data.py
@@ -3,20 +3,27 @@ from azure.storage.blob import BlobServiceClient
 import pandas as pd
 import io
 
+# Azure Storage
 account_url = "https://uae.blob.core.windows.net"
 container_name = "rehanadataset"
 blob_name = "ctg-studies.json"
+
+# Get secret from Databricks
 account_key = dbutils.secrets.get(scope="azure-storage", key="storage-key")
 
-
+# Connect to Azure Blob Storage
 blob_service_client = BlobServiceClient(account_url, credential=account_key)
 container_client = blob_service_client.get_container_client(container_name)
 
 blob_client = container_client.get_blob_client(blob_name)
+
+# Download JSON
 stream = blob_client.download_blob().readall()
+
+# Read JSON
 df = pd.read_json(io.BytesIO(stream))
 
-# --- 2. Flatten the nested JSON column ---
+#Flatten the nested JSON column 
 df_flat = pd.json_normalize(df['protocolSection'])
 
 # Create a Spark DataFrame from the flattened DataFrame
@@ -28,6 +35,6 @@ spark_df.write \
     .mode("overwrite") \
     .saveAsTable("clinical_trials")
 
-# Display it
+# Display table
 display(spark_df)
 
